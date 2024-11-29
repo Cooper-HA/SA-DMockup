5c0c09754d0edd567eae238918a86c7288dd4146
CatsFromSpace <calicrouse3@gmail.com>
2024-11-29 14:30:38 -0700

    Removed the Java example program, updated with the basic front end layout of the actual project

--- src/main/java/com/acme/demo/db/CountryCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acme.demo.db;
-
-import com.acme.demo.domain.Country;
-
-public interface CountryCustomRepository {
-	public String sayHello();
-	public Country findOneWithDetails(long id);
-	public Country populateOneWithDetails(Country country);
-}
--- src/main/java/com/acme/demo/db/LocationCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.acme.demo.db;
-
-import com.acme.demo.domain.Location;
-
-public interface LocationCustomRepository {
-	public String sayHello();
-	public Location findOneWithDetails(long id);
-	public Location populateOneWithDetails(Location country);
-}
